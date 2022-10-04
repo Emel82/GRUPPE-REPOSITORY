c1fac55cd311d7916f9a009971ba6c9191a5fad2
Chamundsen <simencamu@hotmail.com>
2022-10-04 13:57:55 +0200

    testing av diverse

---
+++ GRUPPE-REPOSITORY/GRUPPE-REPOSITORY/canvas.js
@@ -0,0 +1,18 @@
+var canvas = document.getElementById("canvas");
+
+canvas.width = window.innerWidth; 
+canvas.height = window.innerHeight;
+
+var ctx = canvas.getContext("2d");
+
+
+
+ctx.fillStyle = "#FF9933";
+ctx.fillRect(centreX - 112.5, centreY - 75, 225, 50);
+ctx.filStyle = "#FFFFFF";
+ctx.fillRect(centreX - 112.5, centreY - 75, 225, 50);
+ctx.fillStyle = "#138808";
+ctx.fillRect(centreX - 112.5, centreY - 75, 225, 50);
+
+
+
